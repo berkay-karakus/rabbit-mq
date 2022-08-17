d96aaf868d4f655d3ff6b26236f099cb0a175fe6
berkay-karakus <72767236+berkay-karakus@users.noreply.github.com>
2022-08-17 16:38:29 +0300

    Update Producer.java

--- Producer.java
+++ Producer.java
@@ -1,28 +1,36 @@
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DeliverCallback;
- 
-public class Consumer {
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+import java.nio.charset.Charset;
+public class Producer {
     private final static String QUEUE_NAME1 = "queue1";
-    private final static String QUEUE_NAME2 = "queue2";
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] argv){
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try ( Connection connection = factory.newConnection();
+              Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME1, false, false, false, null);
 
-         ConnectionFactory connectionFactory = new ConnectionFactory();
-         connectionFactory.setHost("localhost");
-         Connection connection = connectionFactory.newConnection();
-         Channel channel = connection.createChannel();
-         channel.queueDeclare(QUEUE_NAME1, false, false, false, null);
+            // 10 günlük hava durumu tahminini al
+            Scanner input = new Scanner(System.in);
+            for(int i = 0; i < 10; i++){
+                int degree = input.nextInt();
+                // queue'ya publish etmek için ayarla
+                String degree_string = degree + "C";
+                // mesajları belirtilen queue'ya gönder
+                channel.basicPublish("", QUEUE_NAME1, null,
+                        degree_string.getBytes(StandardCharsets.UTF_8));
+            }
 
-         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+        } catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
 
-         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-          String message = new String(delivery.getBody(), "UTF-8");
-             System.out.println("[x] Received " + message + "'" );
-            // channel.basicPublish("", QUEUE_NAME2, null, message.getBytes(Charset.forName("UTF-8")));
-         };
-         channel.basicConsume(QUEUE_NAME1, true, deliverCallback, consumerTag -> { });
+        }
+    }
 
-     }
-}
